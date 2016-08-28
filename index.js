a2ec5081792b88da493d755248bd6d89d248b900
Grisanu Naing <mgrisanu@gmail.com>
2016-08-27 23:53:50 -0700

    (fix) Catch edge cases and invalid inputs

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 var makePermutations = function (stringOrArray, number, repetition) {
+  if (stringOrArray.constructor !== String && stringOrArray.constructor !== Array) {
+    throw new Error('Collection must either be a String or an Array');
+  }
+  if (!Number.isInteger(number)) { throw new Error('Selection size must be an integer'); }
+  if (number <= 0 || number > stringOrArray.length) { throw new Error('Selection size must be greater than 0 and less than collection size'); }
+
+  repetition = repetition || false;
+  if (repetition.constructor !== Boolean) { throw new Error('Repetition must be a boolean value'); }
+
+
   if (!repetition) {
     // change str to arr
     var input = stringOrArray.constructor === String ? stringOrArray.split('') : stringOrArray;
@@ -29,6 +39,7 @@ var makePermutations = function (stringOrArray, number, repetition) {
   }
 };
 
-console.log(makePermutations('abc', 2, false));
-console.log(makePermutations('abcd', 2, false));
-console.log(makePermutations('abcd', 4, false));
+console.log(makePermutations('abc', 2)); // [ 'ab', 'ac', 'ba', 'bc', 'ca', 'cb' ]
+console.log(makePermutations('abcd', 2, false)); //  [ 'ab', 'ac', 'ad', 'ba', 'bc', 'bd', 'ca', 'cb', 'cd', 'da', 'db', 'dc' ]
+console.log(makePermutations('abc', 3, false)); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+console.log(makePermutations('abcd', 4, false)); // [ 'abcd', 'abdc', 'acbd', 'acdb', 'adbc', 'adcb', 'bacd', 'badc', 'bcad', 'bcda', 'bdac', 'bdca', 'cabd', 'cadb', 'cbad', 'cbda', 'cdab', 'cdba', 'dabc', 'dacb', 'dbac', 'dbca', 'dcab', 'dcba' ]
